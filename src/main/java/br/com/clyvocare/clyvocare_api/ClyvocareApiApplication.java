5f9746902eea936e238459d0b58007c21a367d4d
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-05-11 22:04:05 -0300

    Estrutura inicial do projeto.

---
+++ src/main/java/br/com/clyvocare/clyvocare_api/ClyvocareApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.clyvocare.clyvocare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClyvocareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClyvocareApiApplication.class, args);
+	}
+
+}
